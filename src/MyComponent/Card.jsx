f96507acd7b238181aa552b1b4940132238fc2e5
Zahid Shaikh <Zahid.786shaikh@gmail.com>
2022-05-30 20:03:00 +0530

    * Added Mock Data * Displayed Mock Data into Camp.jsx file * Made an API Call using Javascirpt's Fetch Method

--- src/MyComponent/Card.jsx
+++ src/MyComponent/Card.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Card = (props) => {
   return (
     <div>
-        <div className="card">
-            <img src={props.cardImg} alt="" />
-            <h5>{props.title}</h5>
-            <p>{props.para}</p>
-            <NavLink className="btn2" to="/Camp" >{props.btn}</NavLink>
-        </div>
+      <div className="card">
+        <img src={props.cardImg} alt="" />
+        <h5>{props.title}</h5>
+        <p>{props.para}</p>
+        <button className="btn2">{props.btn}</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
